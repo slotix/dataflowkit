e534fe4a945f138c19daffcb550e355580af0fc6
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2017-05-07 02:45:22 +0300

    headers casting to http.header

--- server/caching.mw.go
+++ server/caching.mw.go
@@ -3,9 +3,12 @@ package server
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"time"
 
 	"fmt"
 
+	"github.com/pquerna/cachecontrol/cacheobject"
 	"github.com/slotix/dataflowkit/cache"
 	"github.com/slotix/dataflowkit/parser"
 	"github.com/slotix/dataflowkit/splash"
@@ -52,7 +55,7 @@ func (mw cachemw) Fetch(req splash.Request) (output io.ReadCloser, err error) {
 		return nil, respErr
 	}
 	//Check if it is cacheable
-	rv := cache.Cacheable(resp)
+	rv := cacheable(resp)
 	expTime := rv.OutExpirationTime.Unix()
 	if rv.OutExpirationTime.IsZero() {
 		expTime = 0
@@ -122,4 +125,44 @@ func (mw cachemw) ParseData(payload []byte) (output []byte, err error) {
 		logger.Println(err.Error())
 	}
 	return
+}
+
+//Cacheable check if resource is cacheable
+func cacheable(resp *splash.Response) (rv cacheobject.ObjectResults) {
+
+	respHeader := resp.Response.Headers.(http.Header)
+	reqHeader := resp.Request.Headers.(http.Header)
+
+	reqDir, err := cacheobject.ParseRequestCacheControl(reqHeader.Get("Cache-Control"))
+	if err != nil {
+		logger.Printf(err.Error())
+	}
+	resDir, err := cacheobject.ParseResponseCacheControl(respHeader.Get("Cache-Control"))
+	if err != nil {
+		logger.Printf(err.Error())
+	}
+	//logger.Println(respHeader)
+	expiresHeader, _ := http.ParseTime(respHeader.Get("Expires"))
+	dateHeader, _ := http.ParseTime(respHeader.Get("Date"))
+	lastModifiedHeader, _ := http.ParseTime(respHeader.Get("Last-Modified"))
+	obj := cacheobject.Object{
+		//	CacheIsPrivate:         false,
+		RespDirectives:         resDir,
+		RespHeaders:            respHeader,
+		RespStatusCode:         resp.Response.Status,
+		RespExpiresHeader:      expiresHeader,
+		RespDateHeader:         dateHeader,
+		RespLastModifiedHeader: lastModifiedHeader,
+
+		ReqDirectives: reqDir,
+		ReqHeaders:    reqHeader,
+		ReqMethod:     resp.Request.Method,
+
+		NowUTC: time.Now().UTC(),
+	}
+
+	rv = cacheobject.ObjectResults{}
+	cacheobject.CachableObject(&obj, &rv)
+	cacheobject.ExpirationObject(&obj, &rv)
+	return rv
 }
