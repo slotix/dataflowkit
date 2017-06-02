aa9fdd248cb9a7b558a295195599d7204f7bbde4
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2017-06-03 01:07:13 +0200

    uncommented middleware

--- server/server.go
+++ server/server.go
@@ -7,8 +7,10 @@ import (
 	"time"
 
 	kitlog "github.com/go-kit/kit/log"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/julienschmidt/httprouter"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -23,35 +25,35 @@ func Init(port string) {
 	var serverLogger kitlog.Logger
 	serverLogger = kitlog.NewLogfmtLogger(os.Stderr)
 	serverLogger = kitlog.With(serverLogger, "caller", kitlog.DefaultCaller, "Time", time.Now().Format("Jan _2 15:04:05"))
-/*
+
 	fieldKeys := []string{"method", "error"}
-	
-		requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-			Namespace: "dfk",
-			Subsystem: "parse_service",
-			Name:      "request_count",
-			Help:      "Number of requests received.",
-		}, fieldKeys)
-		requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-			Namespace: "dfk",
-			Subsystem: "parse_service",
-			Name:      "request_latency_microseconds",
-			Help:      "Total duration of requests in microseconds.",
-		}, fieldKeys)
-		countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-			Namespace: "dfk",
-			Subsystem: "parse_service",
-			Name:      "count_result",
-			Help:      "The result of each count method.",
-		}, []string{})
-	*/
+
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "dfk",
+		Subsystem: "parse_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "dfk",
+		Subsystem: "parse_service",
+		Name:      "request_latency_microseconds",
+		Help:      "Total duration of requests in microseconds.",
+	}, fieldKeys)
+	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+		Namespace: "dfk",
+		Subsystem: "parse_service",
+		Name:      "count_result",
+		Help:      "The result of each count method.",
+	}, []string{})
+
 	var svc ParseService
 	svc = parseService{}
-	//	svc = statsMiddleware("18")(svc)
-	//svc = cachingMiddleware()(svc)
+	svc = statsMiddleware("18")(svc)
+	svc = cachingMiddleware()(svc)
 	svc = loggingMiddleware(serverLogger)(svc)
-	//	svc = robotsTxtMiddleware()(svc)
-	//	svc = instrumentingMiddleware(requestCount, requestLatency, countResult)(svc)
+	svc = robotsTxtMiddleware()(svc)
+	svc = instrumentingMiddleware(requestCount, requestLatency, countResult)(svc)
 
 	fetchHandler := httptransport.NewServer(
 		makeFetchEndpoint(svc),
