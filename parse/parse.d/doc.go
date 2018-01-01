3d54c229e0a47de86214bc6c7a51d8ca70189a31
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-02 00:50:38 +0100

    godoc.org output test

--- parse/parse.d/doc.go
+++ parse/parse.d/doc.go
@@ -6,11 +6,11 @@
 // All rights reserved. Use of this source code is governed
 // by the BSD 3-Clause License license.
 
-// Parse service of the Dataflow kit parses html content from web pages following the rules described in Payload JSON file. 
-// Examples for accessing Parse endpoint are here :
-//
+/*
+Parse service of the Dataflow kit parses html content from web pages following the rules described in Payload JSON file.
+Examples for accessing Parse endpoint are here :
 
-/* 		{
+		{
 		"name":"collection",
 		"request":{
 			"url":"https://example.com"
@@ -51,8 +51,8 @@
 		},
 		"format":"json",
 		"paginateResults":false
-		} */
-//
+		}
+*/
 package main
 
 // EOF
