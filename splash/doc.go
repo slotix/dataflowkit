cc34e1a7c08a0dea9e2d0a0bc82b611abfd7e0a6
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-02 23:47:04 +0100

    reformated doc.go

--- splash/doc.go
+++ splash/doc.go
@@ -8,13 +8,12 @@
 //
 // Package splash of the Dataflow kit is for accessing remote Splash server which is used  for fetching html web pages. 
 //
-//Sometimes it is not enough to just download page source. Actual data can't be retrieved without running javascript code on a downloaded page first. 
+// Sometimes it is not enough to just download page source. Actual data can't be retrieved without running javascript code on a downloaded page first. 
 //
-//Splash is a javascript rendering service. It is used by Dataflow kit as a fetcher in opposite to Base fetcher which just download web page content as is.  
+// Splash is a javascript rendering service. It is used by Dataflow kit as a fetcher in opposite to Base fetcher which just download web page content as is.  
 //
 // Read more at https://github.com/scrapinghub/splash
 //
-
 package splash
 
 // EOF
