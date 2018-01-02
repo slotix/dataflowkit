0b119e8e0826dea34c826f07f70d772357644e3a
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-02 22:23:55 +0100

    added more doc files

---
+++ webserver/doc.go
@@ -0,0 +1,15 @@
+// Dataflow kit - webserver
+//
+// Copyright © 2017-2018 Slotix s.r.o. <dm@slotix.sk>
+//
+//
+// All rights reserved. Use of this source code is governed
+// by the BSD 3-Clause License license.
+
+// Webserver service of the Dataflow kit serves website for data extraction. It helps Payload JSON generation with point and click interface for feeding Dataflow kit parser.
+//
+//It also passes requests for fetching html web pages and parser endpoints to corresponding services. 
+//
+package main
+
+// EOF
