beef98a80ed8cd4e4f2141fcee9802db6e4a0935
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-02 02:06:58 +0100

    fetch.d doc formatting

--- fetch/fetch.d/doc.go
+++ fetch/fetch.d/doc.go
@@ -7,10 +7,13 @@
 // by the BSD 3-Clause License license.
 
 // Fetcher service of the Dataflow kit downloads html content from web pages to feed Dataflow kit scrapers.
-// Currently two types of fetcher are available : Splash Fetcher and Base Fetcher
-// Base fetcher is used for downloading html web page using Go standard library's http
+//
+// Currently two types of fetcher are available : Splash Fetcher and Base Fetcher.
+//
+// Base fetcher is used for downloading html web page using Go standard library's http.
+//
 // Splash Fetcher uses Scrapinghub splash to fetch URLs.
-//Splash is a javascript rendering service
+// Splash is a javascript rendering service.
 // Read more at https://github.com/scrapinghub/splash
 //
 // Here are two Examples for accessing Fetcher endpoints:
