debb213724f790bc72492d29881d851e2ae9f46d
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-07 02:44:03 +0100

    mocked S3 tests

--- storage/s3.go
+++ storage/s3.go
@@ -2,14 +2,14 @@ package storage
 
 import (
 	"bytes"
-	"errors"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
 )
 
 //S3Conn represents a AWS S3 Connection structure
@@ -31,30 +31,40 @@ func newS3Conn(config *aws.Config, bucket string) S3Conn {
 	return S3Conn{config, bucket}
 }
 
-//download returns a value of specified key from AWS S3
-func (c S3Conn) download(key string) (value []byte, err error) {
-	sess := session.Must(session.NewSession(c.config))
-	downloader := s3manager.NewDownloader(sess)
+//download returns a value of specified key from AWS S3 bucket.
+// s3manageriface.DownloaderAPI interface helps to mock real S3 connection.
+func download(d s3manageriface.DownloaderAPI, bucket, key string) ([]byte, error) {
+	//downloader := s3manager.NewDownloaderWithClient(svc)
 	buf := &aws.WriteAtBuffer{}
-	_, err = downloader.Download(buf,
-		&s3.GetObjectInput{
-			Bucket: aws.String(c.bucket),
-			Key:    aws.String(key),
-		})
+	object := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+	_, err := d.Download(buf, object)
 	if err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
 }
 
-//upload sends key/value pair to AWS S3 Storage
-func (c S3Conn) upload(key string, value []byte, expTime int64) error {
+//download returns a value of specified key from AWS S3
+func (c S3Conn) download(key string) (value []byte, err error) {
 	sess := session.Must(session.NewSession(c.config))
-	uploader := s3manager.NewUploader(sess)
+	//svc := s3.New(sess)
+	downloader := s3manager.NewDownloader(sess)
+	value, err = download(downloader, c.bucket, key)
+
+	return
+}
+
+//upload uploads key/value pair to AWS S3 Storage and sets up Expires parameter
+// s3manageriface.UploaderAPI helps to mock real S3 connection.
+func upload(u s3manageriface.UploaderAPI, bucket, key string, value []byte, expTime int64) error {
+	//uploader := s3manager.NewUploaderWithClient(svc)
 	r := bytes.NewReader(value)
 	t := time.Unix(expTime, 0)
-	_, err := uploader.Upload(&s3manager.UploadInput{
-		Bucket:  aws.String(c.bucket),
+	_, err := u.Upload(&s3manager.UploadInput{
+		Bucket:  aws.String(bucket),
 		Key:     aws.String(key),
 		Body:    r,
 		Expires: &t,
@@ -65,40 +75,63 @@ func (c S3Conn) upload(key string, value []byte, expTime int64) error {
 	return nil
 }
 
+//upload uploads key/value pair to AWS S3 Storage and sets up Expires parameter
+func (c S3Conn) upload(key string, value []byte, expTime int64) error {
+	sess := session.Must(session.NewSession(c.config))
+	//svc := s3.New(sess)
+	uploader := s3manager.NewUploader(sess)
+	err := upload(uploader, c.bucket, key, value, expTime)
+	return err
+}
+
 //getObject returns an object from AWS S3. This may be used to get meta information about an object.
 func (c S3Conn) getObject(key string) (object *s3.GetObjectOutput, err error) {
 	sess := session.Must(session.NewSession(c.config))
 	svc := s3.New(sess)
+	object, err = getObject(svc, c.bucket, key)
+	return
+}
+
+//getObject returns an object from AWS S3. This may be used to get meta information about an object.
+func getObject(svc s3iface.S3API, bucket, key string) (object *s3.GetObjectOutput, err error) {
+	//sess := session.Must(session.NewSession(c.config))
+	//svc := s3.New(sess)
 	input := &s3.GetObjectInput{
-		Bucket: aws.String(c.bucket),
+		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	}
 	object, err = svc.GetObject(input)
 	return
 }
 
+func listBuckets(svc s3iface.S3API) ([]string, error) {
+	resp, err := svc.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		return nil, err
+	}
+
+	buckets := make([]string, 0, len(resp.Buckets))
+	for _, b := range resp.Buckets {
+		buckets = append(buckets, *b.Name)
+	}
+
+	return buckets, nil
+}
+
+func delete(svc s3iface.S3API, bucket, key string) (result *s3.DeleteObjectOutput, err error) {
+	//sess := session.Must(session.NewSession(c.config))
+	//svc := s3.New(sess)
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+	result, err = svc.DeleteObject(input)
+	return
+}
+
 func (c S3Conn) delete(key string) (result *s3.DeleteObjectOutput, err error) {
 	sess := session.Must(session.NewSession(c.config))
 	svc := s3.New(sess)
-	input := &s3.DeleteObjectInput{
-		Bucket: aws.String(c.bucket),
-		Key:    aws.String(key),
-	}
-
-	result, err = svc.DeleteObject(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				return result, errors.New(aerr.Error())
-
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			return result, errors.New(aerr.Error())
-		}
-		return
-	}
+	result, err = delete(svc, c.bucket, key)
 	return
 }
--- storage/s3_test.go
+++ storage/s3_test.go
@@ -2,20 +2,77 @@ package storage
 
 //http://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/s3-example-basic-bucket-operations.html
 import (
-	"fmt"
-	"log"
-	"os/user"
 	"testing"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/spf13/viper"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/slotix/dataflowkit/storage/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
+func TestUpload(t *testing.T) {
+	uploader := new(mocks.UploaderAPI)
+	uploader.On("Upload", mock.AnythingOfType("*s3manager.UploadInput")).Return(&s3manager.UploadOutput{
+		Location: "location",
+	}, nil)
+
+	err := upload(uploader, "bucket", "key", []byte("value"), 0)
+	assert.Nil(t, err, "Expected no error")
+}
+
+func TestDownload(t *testing.T) {
+	downloader := new(mocks.DownloaderAPI)
+	buf := &aws.WriteAtBuffer{}
+	//value := []byte("Value")
+	//buf.WriteAt(value, int64(len(value)))
+	//logger.Info(string(buf.Bytes()))
+	//buf := aws.NewWriteAtBuffer([]byte("Value"))
+
+	obj := &s3.GetObjectInput{
+		Bucket: aws.String("bucket"),
+		Key:    aws.String("key"),
+	}
+	downloader.On("Download", buf, obj).Return(int64(0), nil)
+	_, err := download(downloader, "bucket", "key")
+	assert.Nil(t, err, "Expected no error")
+}
+
+func TestListBuckets(t *testing.T) {
+	svc := new(mocks.S3API)
+	svc.On("ListBuckets", mock.AnythingOfType("*s3.ListBucketsInput")).Return(&s3.ListBucketsOutput{
+		Buckets: []*s3.Bucket{
+			&s3.Bucket{Name: aws.String("First Bucket")},
+			&s3.Bucket{Name: aws.String("Second Bucket")},
+		},
+	}, nil)
+
+	b, err := listBuckets(svc)
+	assert.Nil(t, err, "Expected no error")
+	assert.Len(t, b, 2, "Expect two buckets")
+	assert.Equal(t, "First Bucket", b[0], "Expected first bucket")
+	assert.Equal(t, "Second Bucket", b[1], "Expected Second Bucket")
+}
+
+func TestGetObject(t *testing.T) {
+	svc := new(mocks.S3API)
+	svc.On("GetObject", mock.AnythingOfType("*s3.GetObjectInput")).Return(&s3.GetObjectOutput{}, nil)
+
+	_, err := getObject(svc, "bucket", "key")
+	assert.Nil(t, err, "Expected no error")
+}
+
+func TestDelete(t *testing.T) {
+	svc := new(mocks.S3API)
+	svc.On("DeleteObject", mock.AnythingOfType("*s3.DeleteObjectInput")).Return(&s3.DeleteObjectOutput{}, nil)
+
+	_, err := delete(svc, "bucket", "key")
+	assert.Nil(t, err, "Expected no error")
+}
+
+//Actual tests *******
+/*
 var (
 	conn S3Conn
 )
@@ -26,41 +83,44 @@ func init() {
 	viper.Set("DFK_BUCKET", "dfk-storage")
 	bucket := viper.GetString("DFK_BUCKET")
 	config := &aws.Config{
-		Credentials: credentials.NewSharedCredentials(viper.GetString("SPACES_CONFIG"), ""), //Load credentials from specified file
-		Endpoint:    aws.String(viper.GetString("SPACES_ENDPOINT")),                         //Endpoint is obligatory for DO Spaces
-		Region:      aws.String("ams333"),                                                   //Actually for Digital Ocean spaces region parameter may have any value. But it can't be omitted.
+		//Load credentials from specified file
+		Credentials: credentials.NewSharedCredentials(viper.GetString("SPACES_CONFIG"), ""),
+		//Endpoint is obligatory for DO Spaces
+		Endpoint:    aws.String(viper.GetString("SPACES_ENDPOINT")),
+		//Region parameter may have any value for Digital Ocean spaces. But it can't be omitted.
+		Region:      aws.String("ams333"),
 	}
-
 	conn = newS3Conn(config, bucket)
-	/* bucket = "fetch-bucket"
-	// Initialize a session that the SDK will use to load configuration,
-	// credentials, and region from the shared config file. (~/.aws/config).
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-
-	// Create S3 service client
-	svc = s3.New(sess)
-	uploader = s3manager.NewUploader(sess)
-	downloader = s3manager.NewDownloader(sess) */
-
 }
 
-func TestListBuckets(t *testing.T) {
+func TestListBucketsAWS(t *testing.T) {
+	config := &aws.Config{Region: aws.String("us-west-1")}
+	sess := session.Must(session.NewSession(config))
+	svc := s3.New(sess)
+	ifaceClient := s3iface.S3API(svc)
+	buckets, err := listBuckets(ifaceClient)
+	if err != nil {
+		log.Fatalln("Failed to list buckets", err)
+	}
+
+	fmt.Println("Buckets:", buckets)
+}
+
+func TestListBucketsDO(t *testing.T) {
 	sess := session.Must(session.NewSession(conn.config))
 	svc := s3.New(sess)
-	result, err := svc.ListBuckets(nil)
-
+	//result, err := svc.ListBuckets(nil)
+	result, err := listBuckets(svc)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("Buckets:")
+	fmt.Println("Buckets:", result)
 
-	for _, b := range result.Buckets {
-		fmt.Printf("* %s created on %s\n",
-			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
-	}
+	// for _, b := range result.Buckets {
+	// 	fmt.Printf("* %s created on %s\n",
+	// 		aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
+	// }
 
 }
 
@@ -86,37 +146,37 @@ func TestUploadDownloadDelete(t *testing.T) {
 	//Test upload
 	buf := []byte("file content test\nanother line of test here")
 	err := conn.upload("test", buf, 0)
-	assert.Equal(t, err, nil)
+	assert := assert.New(t)
+	assert.Equal(err, nil)
 
 	//Test download
 	downloaded, _ := conn.download("test")
-	assert.Equal(t, buf, downloaded)
+	assert.Equal(buf, downloaded)
 
 	//Test Get Object
 	object, _ := conn.getObject("test")
-	//fmt.Println(*object)
-	assert.NotNil(t, object)
+	//	fmt.Println(object.String())
+	assert.NotNil(object)
 
 	//Test delete
 	_, err = conn.delete("test")
-	assert.Nil(t, err)
+	assert.Nil(err)
 
 	//Test Downloading of item with Invalid Key recently deleted
 	downloaded, err = conn.download("test")
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
-			assert.Equal(t, aerr.Code(), "NoSuchKey")
+			assert.Equal(aerr.Code(), "NoSuchKey")
 		}
 	}
 	//Test Get Object with Invalid Key recently deleted
 	_, err = conn.getObject("test")
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
-			assert.Equal(t, aerr.Code(), "NoSuchKey")
+			assert.Equal(aerr.Code(), "NoSuchKey")
 		}
 	}
 }
-
 
 //homeDir returns user's $HOME directory
 func homeDir() string {
@@ -126,3 +186,6 @@ func homeDir() string {
 	}
 	return usr.HomeDir + "/"
 }
+
+*/
+//**********
