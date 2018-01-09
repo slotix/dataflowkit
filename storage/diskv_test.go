7e8cb8e14e72baa98efd85543c3ccdf521d7a4aa
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-09 02:50:00 +0100

    more tests added to Splash. Cookies changes

--- storage/diskv_test.go
+++ storage/diskv_test.go
@@ -3,7 +3,6 @@ package storage
 import (
 	"testing"
 
-	"github.com/peterbourgon/diskv"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
@@ -29,49 +28,49 @@ func Test_diskv(t *testing.T) {
 	//Add two values to storage
 	err = d.Write(testKey, testValue, 0)
 	err = d.Write("secondKey", []byte("testValue"), 0)
-	//erase all items 
+	//erase all items
 	err = d.EraseAll()
 	assert.NoError(t, err, "Expected no error")
 }
 
-func TestDiskvConn_Write1(t *testing.T) {
-	diskvConn := newDiskvConn("", 1024*1024)
-	type fields struct {
-		diskv *diskv.Diskv
-	}
-	type args struct {
-		key     string
-		value   []byte
-		expTime int64
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
-	}{
-		{name: "qq",
-			fields: fields{
-				diskv: diskvConn.diskv,
-			},
-			args: args{
-				key:     "testKey",
-				value:   []byte("testValue"),
-				expTime: 0,
-			},
-			wantErr: false,
-		},
-	}
-	// TODO: Add test cases.
+// func TestDiskvConn_Write1(t *testing.T) {
+// 	diskvConn := newDiskvConn("", 1024*1024)
+// 	type fields struct {
+// 		diskv *diskv.Diskv
+// 	}
+// 	type args struct {
+// 		key     string
+// 		value   []byte
+// 		expTime int64
+// 	}
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		wantErr bool
+// 	}{
+// 		{name: "qq",
+// 			fields: fields{
+// 				diskv: diskvConn.diskv,
+// 			},
+// 			args: args{
+// 				key:     "testKey",
+// 				value:   []byte("testValue"),
+// 				expTime: 0,
+// 			},
+// 			wantErr: false,
+// 		},
+// 	}
+// 	// TODO: Add test cases.
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			d := DiskvConn{
-				diskv: tt.fields.diskv,
-			}
-			if err := d.Write(tt.args.key, tt.args.value, tt.args.expTime); (err != nil) != tt.wantErr {
-				t.Errorf("DiskvConn.Write() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			d := DiskvConn{
+// 				diskv: tt.fields.diskv,
+// 			}
+// 			if err := d.Write(tt.args.key, tt.args.value, tt.args.expTime); (err != nil) != tt.wantErr {
+// 				t.Errorf("DiskvConn.Write() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
+// 		})
+// 	}
+// }
