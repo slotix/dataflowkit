f11a3347716d720944e0eeb3be403929c6e513c9
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-01-15 23:02:32 +0100

    fix robots.txt tests

--- fetch/robotstxt_test.go
+++ fetch/robotstxt_test.go
@@ -2,9 +2,10 @@ package fetch
 
 import (
 	"testing"
+	"time"
 
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
+	"github.com/temoto/robotstxt"
 )
 
 func TestIsRobotsTxt(t *testing.T) {
@@ -14,6 +15,15 @@ func TestIsRobotsTxt(t *testing.T) {
 }
 
 func TestRobotstxtData(t *testing.T) {
+	robots, err := robotstxt.FromBytes(robotstxtData)
+	assert.NoError(t, err, "No error returned")
+	assert.Equal(t, true, AllowedByRobots("http://"+addr+"/allowed", robots), "Test allowed url")
+	assert.Equal(t, false, AllowedByRobots("http://"+addr+"/disallowed", robots), "Test disallowed url")
+	assert.Equal(t, time.Duration(0), GetCrawlDelay(robots))
+
+}
+
+/* func TestRobotstxtData(t *testing.T) {
 	viper.Set("DFK_FETCH", "localhost:8000")
 	robots, err := RobotstxtData("http://" + addr)
 	assert.NoError(t, err, "No error returned")
@@ -21,3 +31,4 @@ func TestRobotstxtData(t *testing.T) {
 	assert.Equal(t, false, AllowedByRobots("http://"+addr+"/disallowed", robots), "Test disallowed url")
 
 }
+*/
