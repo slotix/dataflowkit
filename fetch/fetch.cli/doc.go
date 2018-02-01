e88e49f8bda9cd0f83ff77147c49fd17201f701f
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-02-01 21:41:53 +0100

    add more parameters to fetch.cli

--- fetch/fetch.cli/doc.go
+++ fetch/fetch.cli/doc.go
@@ -22,11 +22,20 @@
 //		./fetch.cli --URL http://example.com
 //		./fetch.cli --URL http://example.com --FETCHER_TYPE base
 //		./fetch.cli -u http://example.com -t base
+//		./fetch.cli -u http://example.com -t splash
 //
 // Flags and configuration settings
 //		DFK_FETCH: HTTP listen address of Fetch service (defaults to "127.0.0.1:8000")
-//		FETCHER_TYPE: DFK Fetcher type: splash, base
+//		FETCHER_TYPE: DFK Fetcher type: splash, base (defaults to splash)
 //		URL: URL to be fetched
+// Request parameters for splash fetcher
+//		PARAMS: Params is a string value for passing formdata parameters.
+//		For example the following string for processing pages which
+//		require authentication may be passed:
+//		"auth_key=880ea6a14ea49e853634fbdc5015a024&referer=http%3A%2F%2Fexample.com%2F&ips_username=user&ips_password=userpassword&rememberMe=1"
+//		COOKIES: Cookies contain cookies to be added to request  before
+//		sending it to browser.
+//		LUA: LUA Splash custom script. See https://splash.readthedocs.io/en/stable/scripting-overview.html for more information.
 //
 package main
 
