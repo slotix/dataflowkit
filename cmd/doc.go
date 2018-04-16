f91afb81eea107d4c695d8942bfd4bc65442a93b
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-04-16 16:55:03 +0200

    add doc to cmd package

---
+++ cmd/doc.go
@@ -0,0 +1,19 @@
+// Dataflow kit - cmd
+//
+// Copyright © 2017-2018 Slotix s.r.o. <dm@slotix.sk>
+//
+//
+// All rights reserved. Use of this source code is governed
+// by the BSD 3-Clause License license.
+
+// Package cmd of the Dataflow kit contains the following CLI daemons: 
+//
+// - fetch.d service downloads html content from web pages to feed Dataflow kit scrapers.
+//
+// - parse.d service parses html content from web pages following the rules described in configuration JSON file.
+//
+// - fetch.cli CLI tool for fetch.d service.
+//
+package cmd
+
+// EOF
