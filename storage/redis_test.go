d795c8f3a0b662a50e83544f90cba40a07d41cbd
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-05-06 22:08:31 +0200

    add tests for redis storage

--- storage/redis_test.go
+++ storage/redis_test.go
@@ -4,77 +4,82 @@ import (
 	"testing"
 	"time"
 
-	"github.com/alicebob/miniredis"
-	"github.com/garyburd/redigo/redis"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
-func init() {
-}
-
 func TestRedis(t *testing.T) {
-	server, err := miniredis.Run()
-	if err != nil {
-		panic(err)
-	}
-	defer server.Close()
+	viper.Set("REDIS", "127.0.0.1:6379")
+	viper.Set("REDIS_NETWORK", "tcp")
+	viper.Set("REDIS_PASSWORD", "")
+	viper.Set("REDIS_DB", "Redis DB")
+	viper.Set("REDIS_SOCKET_PATH", "")
+	//viper.Set("REDIS_EXPIRE", 3600)
+
+	redisCon := NewRedisConn()
+	conn := redisCon.open()
+	//redisCon.conn = &conn
+	defer conn.Close()
+
+	//Delete all values from redis if any
+	err := redisCon.DeleteAll()
+
 	// Optionally set some keys your code expects:
 	testKey := "some key"
 	testValue := []byte("some value")
-	// Run your code and see if it behaves.
-	// An example using the redigo library from "github.com/garyburd/redigo/redis":
-	conn, err := redis.Dial("tcp", server.Addr())
 
-	//Write some values
-	err = setVal(conn, testKey, testValue)
+	//Write some values to Redis
+	err = redisCon.SetValue(testKey, testValue)
 	assert.Nil(t, err, "Expected no error")
-	err = setVal(conn, "numeric key", 18)
+	err = redisCon.SetValue("numeric key", 18)
 	assert.Nil(t, err, "Expected no error")
-	err = setVal(conn, "One more value", 111)
+	
+	//Write
+	//expAt := time.Now().UTC().Add(time.Duration(24 * time.Hour))
+	err = redisCon.Write("One more key", []byte("111"), 3600)
 	assert.Nil(t, err, "Expected no error")
 
 	//Read from Redis
-	value, err := val(conn, testKey)
+	value, err := redisCon.Read(testKey)
 	assert.Nil(t, err, "Expected no error")
 	assert.Equal(t, value, testValue, "Expected value = testValue")
-	numeric, err := intVal(conn, "numeric key")
+	numeric, err := redisCon.ReadInt("numeric key")
 	assert.Nil(t, err, "Expected no error")
 	assert.Equal(t, numeric, int64(18), "Expected numeric = 18")
 
-	expAt := time.Now().UTC().Add(time.Duration(5 * time.Second))
-	err = setExpireAt(conn, testKey, expAt.Unix())
+	//Expirations
+	expIn := int64(50)
+	ttl, err := redisCon.TTL(testKey)
+	t.Log(ttl, err)
+	err = redisCon.ExpireIn(testKey, expIn)
 	assert.Nil(t, err, "Expected no error")
-	TTL, err := ttl(conn, testKey)
+	ttl, err = redisCon.TTL(testKey)
+	t.Log(ttl, err)
 	assert.Nil(t, err, "Expected no error")
-	assert.Equal(t, TTL > 0, true, "Expected TTL >0")
+	assert.Equal(t, true, ttl > 0, "Expected TTL >0")
 
-	expIn := int64(5)
-	err = setExpireIn(conn, testKey, expIn)
+	//EXPIREAT
+	expAt := time.Now().UTC().Add(time.Duration(24 * time.Hour))
+	t.Log(expAt)
+	err = redisCon.SetExpireAt(testKey, expAt.Unix())
 	assert.Nil(t, err, "Expected no error")
-	TTL, err = ttl(conn, testKey)
+	ttl, err = redisCon.TTL(testKey)
+	t.Log(ttl, err)
 	assert.Nil(t, err, "Expected no error")
-	assert.Equal(t, TTL > 0, true, "Expected TTL >0")
-	exp := expired(conn, testKey)
+	assert.Equal(t, true, ttl > 0, "Expected TTL >0")
+
+	exp := redisCon.Expired(testKey)
 	//logger.Info(TTL)
 	assert.Equal(t, exp, false, "Expected Not expired")
-	
-	err = setTTL(conn, testKey, -1)
-	exp = expired(conn, testKey)
+
+	err = redisCon.SetTTL(testKey, -1)
+	exp = redisCon.Expired(testKey)
 	assert.Equal(t, exp, true, "Expected expired value")
 
-	err = deleteKey(conn, "numeric key")
+	err = redisCon.Delete("numeric key")
 	assert.Nil(t, err, "Expected no error")
-	err = deleteAllKeys(conn)
+	err = redisCon.DeleteAll()
 	assert.Nil(t, err, "Expected no error")
-
-}
-
-func TestNewRedisConn(t *testing.T) {
-	rc := NewRedisConn()
-	t.Log(rc)
-	conn := rc.open()
-	t.Log(conn)
-	defer conn.Close()
-	// err := rc.Write("test",[]byte("fff"),0)
-	// t.Log(err)
+	//err := redisCon.Write("test", []byte("fff"), 0)
+	//t.Log(err)
 }
