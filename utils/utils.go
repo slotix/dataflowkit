c6f53555dfd8c27656d9f5f3f77cb62589cf0877
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-05-06 18:46:19 +0200

    add tests to utils package. Remove obsolete functions

--- utils/utils.go
+++ utils/utils.go
@@ -15,12 +15,9 @@ import (
 	"crypto/md5"
 	"hash/crc32"
 	"io"
-	"io/ioutil"
 	"math/rand"
 	"net/url"
-	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -63,80 +60,6 @@ func RelUrl(base, rel string) (string, error) {
 	newUrl := baseUrl.ResolveReference(relUrl)
 	return newUrl.String(), nil
 }
-
-//stringInSlice check if specified string in the slice of strings
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
-// InsertStringToSlice inserts the value into the slice at the specified index,
-// which must be in range.
-// The slice must have room for the new element.
-func InsertStringToSlice(slice []string, index int, value string) []string {
-	// Grow the slice by one element.
-	slice = slice[0 : len(slice)+1]
-	// Use copy to move the upper part of the slice out of the way and open a hole.
-	copy(slice[index+1:], slice[index:])
-	// Store the new value.
-	slice[index] = value
-	// Return the result.
-	return slice
-}
-
-// AddStringSliceToSlice joins two string slices.
-func AddStringSliceToSlice(in []string, out []string) {
-	for _, s := range in {
-		if !stringInSlice(s, out) {
-			out = append(out, s)
-		}
-	}
-}
-
-// ReadLinesOfFile returns the lines from a file as a slice of strings
-func ReadLinesOfFile(filename string) []string {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	lines := strings.Split(string(content), "\n")
-	return lines
-}
-
-// RegSplit splits the strings into strings using the regular expression as separator
-func RegSplit(text string, reg *regexp.Regexp) []string {
-	indexes := reg.FindAllStringIndex(text, -1)
-	laststart := 0
-	result := make([]string, len(indexes)+1)
-	for i, element := range indexes {
-		result[i] = text[laststart:element[0]]
-		laststart = element[1]
-	}
-	result[len(indexes)] = text[laststart:]
-	return result
-}
-
-func newStringReadCloser(s string) dummyReadCloser {
-	return dummyReadCloser{strings.NewReader(s)}
-}
-
-type dummyReadCloser struct {
-	r io.Reader
-}
-
-func (c dummyReadCloser) Read(b []byte) (int, error) {
-	return c.r.Read(b)
-}
-
-func (dummyReadCloser) Close() error {
-	return nil
-}
-
-var _ io.ReadCloser = &dummyReadCloser{}
 
 //Random generates random int64 value
 func Random(min, max int64) int64 {
