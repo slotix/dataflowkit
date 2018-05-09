b7fe7f7c39a2ab61097348bef75b201e61965ba2
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-05-09 12:22:19 +0200

    fix server_test.go

--- fetch/server_test.go
+++ fetch/server_test.go
@@ -6,17 +6,19 @@ import (
 	"time"
 
 	"github.com/slotix/dataflowkit/splash"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_server(t *testing.T) {
 	//start fetch server
-	fetchServer := "127.0.0.1:8000"
+	fetchServer := "127.0.0.1:8002"
 	serverCfg := Config{
 		Host:         fetchServer, //"localhost:5000",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 	}
+	viper.Set("SKIP_STORAGE_MW", true)
 	htmlServer := Start(serverCfg)
 
 	//create HTTPClient to send requests.
@@ -27,8 +29,8 @@ func Test_server(t *testing.T) {
 
 	//send request to base fetcher endpoint
 	req := BaseFetcherRequest{
-		//URL: "http://" + addr,
-		URL: "http://google.com",
+		URL: "http://" + addr,
+		//URL: "http://google.com",
 
 	}
 	html, err := svc.Fetch(req)
@@ -43,15 +45,16 @@ func Test_server(t *testing.T) {
 	//send request to splash fetcher endpoint
 	sReq := splash.Request{
 		//URL: "http://" + addr,
-		URL: "http://google.com",
+		URL: "http://example.com",
 	}
-	r, err := svc.Response(sReq)
+	r, err := svc.Fetch(sReq)
 	if err != nil {
 		logger.Error(err)
 	}
-	//data, err = ioutil.ReadAll(html)
+	//data, err = ioutil.ReadAll(r)
 	assert.NoError(t, err, "Expected no error")
 	assert.NotNil(t, r)
+	//t.Log(string(data))
 	//assert.Equal(t, indexContent, data, "Expected Hello World")
 
 	// //Test forbidden by robots
