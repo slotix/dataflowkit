57d967644144a48c31e5aec541dd2dad15904936
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-05-11 02:18:07 +0200

    fix tests

--- fetch/robotstxt_test.go
+++ fetch/robotstxt_test.go
@@ -1,10 +1,13 @@
 package fetch
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	//"github.com/slotix/dataflowkit/testserver"
+	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/temoto/robotstxt"
@@ -27,12 +30,41 @@ func TestRobotstxtData(t *testing.T) {
 	robots = nil
 	assert.Equal(t, true, AllowedByRobots("http://"+addr+"/allowed", robots), "Test allowed url")
 
-	viper.Set("DFK_FETCH", "http://127.0.0.1:8000")
-	//rd, err := RobotstxtData("http://" + addr)
-	rd, err := RobotstxtData("https://google.com")
+	//start serving content
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Conent-Type", "text/html")
+		w.Write(IndexContent)
+	})
+	r.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Conent-Type", "text/html")
+		w.Write([]byte(RobotsContent))
+	})
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	//rd, err := RobotstxtData("https://google.com")
+	//viper.Set("DFK_FETCH", ts.URL)
+	
+	//start fetch server
+	viper.Set("DFK_FETCH", "127.0.0.1:8000")
+	fetchServer := viper.GetString("DFK_FETCH")
+	serverCfg := Config{
+		Host:         fetchServer,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	viper.Set("SKIP_STORAGE_MW", true)
+	htmlServer := Start(serverCfg)
+
+	////////
+	rd, err := RobotstxtData(ts.URL)
+
 	assert.NoError(t, err, "No error returned")
 	assert.NotNil(t, rd, "Not nil returned")
 
 	rd, err = RobotstxtData("invalid_host")
 	assert.Error(t, err, "error returned")
+
+	htmlServer.Stop()
 }
