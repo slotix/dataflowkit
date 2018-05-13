78eff2feb326b43d3fb926c42b78c3b40ff57fb5
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-05-13 14:25:32 +0200

    add proxy handler

--- fetch/transport_test.go
+++ fetch/transport_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,15 +18,30 @@ func TestHealthCheckHandler(t *testing.T) {
 	handler.ServeHTTP(w, req)
 
 	// Check the status code is what we expect.
-    if status := w.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
-    }
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
 	resp := w.Result()
 	body, _ := ioutil.ReadAll(resp.Body)
 	assert.Equal(t, []byte(`{"alive": true}`), body)
 }
 
+func TestQuery(t *testing.T) {
+	url := "http://localhost/test?q=http%3A%2F%2Fgoogle.com"
+
+	hit := false
+	m := mux.NewRouter()
+	m.Path("/test").Queries("q", "{q}").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hit = true
+	})
+
+	req, _ := http.NewRequest("GET", url, nil)
+	m.ServeHTTP(&httptest.ResponseRecorder{}, req)
+	if !hit {
+		t.Errorf("query did not hit")
+	}
+}
 
 // func TestDecodeSplashFetcherRequest(t *testing.T) {
 // 	ctx := context.Background()
