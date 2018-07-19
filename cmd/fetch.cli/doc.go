0b02578e6e20256e37fc3cae05e3501d75472e10
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-07-19 23:37:59 +0200

    update fetch package documentation

--- cmd/fetch.cli/doc.go
+++ cmd/fetch.cli/doc.go
@@ -8,13 +8,11 @@
 
 // Fetcher CLI of the Dataflow kit downloads html content from web pages via Fetcher service endpoint.
 //
-// Currently two types of fetcher are available : Splash Fetcher and Base Fetcher.
+// Currently two types of fetcher are available : Headless Chrome Fetcher and Base Fetcher.
 //
 // Base fetcher is used for downloading html web page using Go standard library's http.
 //
-// Splash Fetcher uses Scrapinghub splash to fetch URLs.
-// Splash is a javascript rendering service.
-// Read more at https://github.com/scrapinghub/splash
+// Chrome Fetcher connects to Headless Chrome which renders JavaScript pages.
 //
 // Accessing Fetcher endpoints
 //
@@ -22,11 +20,11 @@
 //		./fetch.cli --URL http://example.com
 //		./fetch.cli --URL http://example.com --FETCHER_TYPE base
 //		./fetch.cli -u http://example.com -t base
-//		./fetch.cli -u http://example.com -t splash
+//		./fetch.cli -u http://example.com -t chrome
 //
 // Flags and configuration settings
 //		DFK_FETCH: HTTP listen address of Fetch service (defaults to "127.0.0.1:8000")
-//		FETCHER_TYPE: DFK Fetcher type: splash, base (defaults to splash)
+//		FETCHER_TYPE: DFK Fetcher type: chrome, base (defaults to base)
 //		URL: URL to be fetched
 // Request parameters for splash fetcher
 //		FORMDATA: string value for passing formdata parameters.
@@ -35,7 +33,6 @@
 //		"auth_key=880ea6a14ea49e853634fbdc5015a024&referer=http%3A%2F%2Fexample.com%2F&ips_username=user&ips_password=userpassword&rememberMe=1"
 //		COOKIES: Cookies contain cookies to be added to request  before
 //		sending it to browser.
-//		LUA: LUA Splash custom script. See https://splash.readthedocs.io/en/stable/scripting-overview.html for more information.
 //
 package main
 
