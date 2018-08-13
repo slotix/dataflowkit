5ca3a98bc0319b5415736ce97ba37636c9d070d5
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-08-13 14:59:16 +0200

    gofmted scrape.go

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1,8 +1,5 @@
 package scrape
 
-// The following code was sourced and modified from the
-// https://github.com/andrew-d/goscrape package governed by MIT license.
-//TODO: add paginator to details
 import (
 	"bytes"
 	"encoding/json"
