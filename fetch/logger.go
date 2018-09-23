8c67cf7a76b93020eb0c01c0c16c301e8bc83e32
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2018-09-23 16:24:13 +0200

    add file logger

--- fetch/logger.go
+++ fetch/logger.go
@@ -2,11 +2,37 @@ package fetch
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var logger *zap.Logger
 
+func NewFileLogger(fNames ...string) *zap.Logger {
+	//logger, _ := zap.NewProduction()
+	cfg := zap.NewProductionConfig()
+	//cfg.OutputPaths = []string{"info.log"}
+	cfg.OutputPaths = fNames
+	//cfg.DisableCaller = true
+	cfg.DisableStacktrace = true
+	encoderCfg := zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		NameKey:        "fetcher",
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+	cfg.EncoderConfig = encoderCfg
+	//cfg.EncoderConfig.TimeKey = ""
+	//cfg.EncoderConfig.LevelKey = ""
+
+	logger, _ := cfg.Build()
+	return logger
+}
+
 func init() {
 	logger, _ = zap.NewDevelopment()
-
+	//logger = NewFileLogger("info.log")
 }
