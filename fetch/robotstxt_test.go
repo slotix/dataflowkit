9458b0bf8caaf3e5469a7cc2db0277a7c1439621
sleeper <sleeper.ok@gmail.com>
2019-01-11 18:50:10 +0600

    fixed robotstxt test

--- fetch/robotstxt_test.go
+++ fetch/robotstxt_test.go
@@ -22,7 +22,7 @@ func TestRobotstxtData(t *testing.T) {
 	assert.NoError(t, err, "No error returned")
 	assert.Equal(t, true, AllowedByRobots("http://"+addr+"/allowed", robots), "Test allowed url")
 	assert.Equal(t, false, AllowedByRobots("http://"+addr+"/disallowed", robots), "Test disallowed url")
-	assert.Equal(t, time.Duration(0), getCrawlDelay(robots))
+	assert.Equal(t, time.Duration(0), GetCrawlDelay(robots))
 	robots = nil
 	assert.Equal(t, true, AllowedByRobots("http://"+addr+"/allowed", robots), "Test allowed url")
 	serverCfg := Config{
