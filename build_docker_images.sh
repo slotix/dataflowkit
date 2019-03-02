18260129122957bfe4a2d4950403c3ab57dfe05a
DMITRY NARIZHNYKH <dmsoft@gmail.com>
2019-03-02 02:01:34 +0100

    change build scripts

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -2,9 +2,9 @@
 # docker build --rm -f Dockerfile-binary -t 'slotix/dfk-binary' .
 # docker build --rm -f cmd/fetch.d/Dockerfile -t 'slotix/dfk-fetch' .
 # docker build --rm -f cmd/parse.d/Dockerfile -t 'slotix/dfk-parse' .
-cd cmd/parse.d && make container
-cd ../fetch.d && make container
-cd ../../ && docker build --rm -f testserver/Dockerfile -t 'slotix/dfk-testserver' .
+cd cmd/parse.d && make push
+cd ../fetch.d && make push
+cd ../../testserver && make push
 
 
 
