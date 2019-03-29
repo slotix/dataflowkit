df8f9ee0b031e2961b9dbb1eb044e9d8704f4545
sleeper <sleeper.ok@gmail.com>
2019-03-29 17:46:04 +0600

    improve cancelation

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -345,7 +345,7 @@ func (task *Task) scrapeContent(ctx context.Context) error {
 			fetchChannel <- flow{"", "", payload.Request}
 			errc = task.saveIntermediate(ctx, scrapedChannel)
 			errs = append(errs, errc)
-			WaitForPipeline(errs...)
+			waitForPipeline(errs...)
 			task.jobDone.Done()
 		}
 	}
@@ -380,7 +380,11 @@ func (task *Task) fetch(ctx context.Context, in <-chan flow, uid string) (<-chan
 						task.mx.Lock()
 						task.responseCount++
 						task.mx.Unlock()
-						contentChannel <- flow{fmt.Sprintf("%s", uid), request.URL, content}
+						select {
+						case contentChannel <- flow{fmt.Sprintf("%s", uid), request.URL, content}:
+						case <-ctx.Done():
+							return
+						}
 					}
 				}
 			case <-ctx.Done():
@@ -399,60 +403,59 @@ func (task *Task) paginate(ctx context.Context, in <-chan flow, nextPageSelector
 		defer close(errc)
 		currentPageNum := 0
 		for data := range in {
-			select {
-			case <-ctx.Done():
+			if nextPageSelector == "" {
+				data.key = fmt.Sprintf("%s-%d", data.key, currentPageNum)
+				contentChannel <- data
+				close(fetcherChannel)
 				return
-			default:
-				if nextPageSelector == "" {
-					data.key = fmt.Sprintf("%s-%d", data.key, currentPageNum)
-					contentChannel <- data
-					close(fetcherChannel)
+			}
+			content := data.data.(io.ReadCloser)
+			doc, err := goquery.NewDocumentFromReader(content)
+			if err != nil {
+				errc <- errs.ParseError{data.url, err}
+				continue
+			}
+			// feed parser with data
+			selectionContent, _ := goquery.OuterHtml(doc.Selection)
+			contentChannel <- flow{fmt.Sprintf("%s-%d", data.key, currentPageNum), data.url, ioutil.NopCloser(strings.NewReader(selectionContent))}
+			f := Field{CSSSelector: nextPageSelector, Attrs: []string{"href"}, Name: "paginator"}
+			paginator := make(map[string]interface{})
+			err = f.extract(doc.Selection, &paginator, task.templateRequest.URL) /* tw.scraper.Paginator.NextPage(url, doc.Selection) */
+			if err != nil {
+				errc <- errs.ParseError{data.url, err}
+				close(fetcherChannel)
+				return
+			}
+			// Repeat until we don't have any more URLs, or until we hit our page limit.
+			if len(paginator) != 0 &&
+				viper.GetInt("MAX_PAGES") > 0 && currentPageNum < viper.GetInt("MAX_PAGES")-1 {
+				// TODO clone request to use same settings
+				currentPageNum++
+				paginatorLink := ""
+				switch paginator["paginator_href"].(type) {
+				case string:
+					paginatorLink = paginator["paginator_href"].(string)
+				case []string:
+					paginatorLink = paginator["paginator_href"].([]string)[0]
+				}
+				select {
+				case <-ctx.Done():
 					return
+
+				case fetcherChannel <- flow{fmt.Sprintf("%s-%d", data.key, currentPageNum), data.url,
+					fetch.Request{
+						Actions:   "",
+						FormData:  "",
+						Method:    "",
+						Type:      "",
+						URL:       paginatorLink,
+						UserToken: "",
+					},
+				}:
 				}
-				content := data.data.(io.ReadCloser)
-				doc, err := goquery.NewDocumentFromReader(content)
-				if err != nil {
-					errc <- errs.ParseError{data.url, err}
-					continue
-				}
-				// feed parser with data
-				selectionContent, _ := goquery.OuterHtml(doc.Selection)
-				contentChannel <- flow{fmt.Sprintf("%s-%d", data.key, currentPageNum), data.url, ioutil.NopCloser(strings.NewReader(selectionContent))}
-				f := Field{CSSSelector: nextPageSelector, Attrs: []string{"href"}, Name: "paginator"}
-				paginator := make(map[string]interface{})
-				err = f.extract(doc.Selection, &paginator, task.templateRequest.URL) /* tw.scraper.Paginator.NextPage(url, doc.Selection) */
-				if err != nil {
-					errc <- errs.ParseError{data.url, err}
-					close(fetcherChannel)
-					return
-				}
-				// Repeat until we don't have any more URLs, or until we hit our page limit.
-				if len(paginator) != 0 &&
-					viper.GetInt("MAX_PAGES") > 0 && currentPageNum < viper.GetInt("MAX_PAGES")-1 {
-					// TODO clone request to use same settings
-					currentPageNum++
-					paginatorLink := ""
-					switch paginator["paginator_href"].(type) {
-					case string:
-						paginatorLink = paginator["paginator_href"].(string)
-					case []string:
-						paginatorLink = paginator["paginator_href"].([]string)[0]
-					}
-					logger.Info(paginatorLink)
-					fetcherChannel <- flow{fmt.Sprintf("%s-%d", data.key, currentPageNum), data.url,
-						fetch.Request{
-							Actions:   "",
-							FormData:  "",
-							Method:    "",
-							Type:      "",
-							URL:       paginatorLink,
-							UserToken: "",
-						},
-					}
-				} else {
-					close(fetcherChannel)
-					return
-				}
+			} else {
+				close(fetcherChannel)
+				return
 			}
 		}
 	}()
@@ -469,64 +472,63 @@ func (task *Task) divide(ctx context.Context, in <-chan flow, fields []Field) (<
 		defer close(blockChannel)
 		defer close(errc)
 		for data := range in {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				content := data.data.(io.ReadCloser)
-				doc, err := goquery.NewDocumentFromReader(content)
-				if err != nil {
-					errc <- errs.ParseError{data.url, err}
-					continue
-				}
-				var selectorAncestor *goquery.Selection
-				index := -1
-				for i, field := range fields {
-					selectorAncestor = doc.Find(field.CSSSelector).First().Parent()
-					if selectorAncestor.Length() > 0 {
-						index = i
-						break
-					}
-				}
-				if index < 0 {
-					errc <- errs.ParseError{URL: data.url, Err: errors.New(errs.ErrNoSelectors)}
-					continue
-				}
-				bFound := false
-				selectorsSlice := fields[index+1:]
-				if len(selectorsSlice) > 0 {
-					for !bFound {
-						for _, f := range selectorsSlice {
-							sel := doc.Find(f.CSSSelector).First()
-							sel = sel.ParentsUntilSelection(selectorAncestor).Last()
-							//check last node.. if it equal html its mean that first selector's parent
-							//not found
-							if goquery.NodeName(sel) == "html" {
-								selectorAncestor = doc.FindSelection(selectorAncestor.Parent().First())
-								bFound = false
-								break
-							}
-							bFound = true
-						}
-					}
-				}
-				fullPath := goquery.NodeName(selectorAncestor)
-				parents := selectorAncestor.ParentsUntilSelection(doc.Find("body"))
-				parents.Each(func(i int, s *goquery.Selection) {
-					//avoid antiscrapin' tech like twitter
-					selector := attrOrDataValue(s)
-					fullPath = selector + " > " + fullPath
-				})
-				items := doc.Find(fullPath)
-				if items.Length() == 0 {
-					errc <- errs.ParseError{data.url, errors.New("No blocks found")}
-					continue
-					//return nil, &errs.BadPayload{ErrText: "No blocks found"}
-				}
-				items.Each(func(i int, s *goquery.Selection) {
-					blockChannel <- flow{data.key, data.url, s}
-				})
+			content := data.data.(io.ReadCloser)
+			doc, err := goquery.NewDocumentFromReader(content)
+			if err != nil {
+				errc <- errs.ParseError{data.url, err}
+				continue
 			}
+			var selectorAncestor *goquery.Selection
+			index := -1
+			for i, field := range fields {
+				selectorAncestor = doc.Find(field.CSSSelector).First().Parent()
+				if selectorAncestor.Length() > 0 {
+					index = i
+					break
+				}
+			}
+			if index < 0 {
+				errc <- errs.ParseError{URL: data.url, Err: errors.New(errs.ErrNoSelectors)}
+				continue
+			}
+			bFound := false
+			selectorsSlice := fields[index+1:]
+			if len(selectorsSlice) > 0 {
+				for !bFound {
+					for _, f := range selectorsSlice {
+						sel := doc.Find(f.CSSSelector).First()
+						sel = sel.ParentsUntilSelection(selectorAncestor).Last()
+						//check last node.. if it equal html its mean that first selector's parent
+						//not found
+						if goquery.NodeName(sel) == "html" {
+							selectorAncestor = doc.FindSelection(selectorAncestor.Parent().First())
+							bFound = false
+							break
+						}
+						bFound = true
+					}
+				}
+			}
+			fullPath := goquery.NodeName(selectorAncestor)
+			parents := selectorAncestor.ParentsUntilSelection(doc.Find("body"))
+			parents.Each(func(i int, s *goquery.Selection) {
+				//avoid antiscrapin' tech like twitter
+				selector := attrOrDataValue(s)
+				fullPath = selector + " > " + fullPath
+			})
+			items := doc.Find(fullPath)
+			if items.Length() == 0 {
+				errc <- errs.ParseError{data.url, errors.New("No blocks found")}
+				continue
+				//return nil, &errs.BadPayload{ErrText: "No blocks found"}
+			}
+			items.Each(func(i int, s *goquery.Selection) {
+				select {
+				case <-ctx.Done():
+					return
+				case blockChannel <- flow{data.key, data.url, s}:
+				}
+			})
 		}
 	}()
 	return blockChannel, errc, nil
@@ -547,33 +549,39 @@ func (task *Task) parse(ctx context.Context, in <-chan flow, fields []Field, isP
 		defer close(result)
 		defer close(errc)
 		for data := range in {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				block := data.data.(*goquery.Selection)
-				blockResult := make(map[string]interface{})
-				for _, field := range fields {
+			block := data.data.(*goquery.Selection)
+			blockResult := make(map[string]interface{})
+			for _, field := range fields {
 
-					if isPath && strings.ToLower(field.Attrs[0]) != "path" {
-						continue
+				if isPath && strings.ToLower(field.Attrs[0]) != "path" {
+					continue
+				}
+				err := field.extract(block, &blockResult, task.templateRequest.URL)
+				if err != nil {
+					errc <- err
+					continue
+				}
+				if len(field.Details.Fields) > 0 {
+					var detailsURL interface{}
+					if !isPath {
+						detailsURL = blockResult[field.Name+"_href"]
+					} else {
+						detailsURL = blockResult[field.Name+"_path"]
+						field.Details.PayloadMD5 = task.rootUID
 					}
-					err := field.extract(block, &blockResult, task.templateRequest.URL)
-					if err != nil {
-						errc <- err
-						continue
-					}
-					if len(field.Details.Fields) > 0 {
-						var detailsURL interface{}
-						if !isPath {
-							detailsURL = blockResult[field.Name+"_href"]
+					switch detailsURL.(type) {
+					case string:
+						field.Details.Request.URL = detailsURL.(string)
+						task.jobDone.Add(1)
+						if isPath {
+							field.Details.blockCounter = blockCounter
 						} else {
-							detailsURL = blockResult[field.Name+"_path"]
-							field.Details.PayloadMD5 = task.rootUID
+							field.Details.InitUID()
 						}
-						switch detailsURL.(type) {
-						case string:
-							field.Details.Request.URL = detailsURL.(string)
+						task.payloads <- field.Details
+					case []string:
+						for _, url := range detailsURL.([]string) {
+							field.Details.Request.URL = url
 							task.jobDone.Add(1)
 							if isPath {
 								field.Details.blockCounter = blockCounter
@@ -581,54 +589,47 @@ func (task *Task) parse(ctx context.Context, in <-chan flow, fields []Field, isP
 								field.Details.InitUID()
 							}
 							task.payloads <- field.Details
-						case []string:
-							for _, url := range detailsURL.([]string) {
-								field.Details.Request.URL = url
-								task.jobDone.Add(1)
-								if isPath {
-									field.Details.blockCounter = blockCounter
-								} else {
-									field.Details.InitUID()
-								}
-								task.payloads <- field.Details
-							}
 						}
-						// save reference to  details uid to be able restore it from storage
-						if !isPath {
-							blockResult[field.Name+"_details"] = field.Details.PayloadMD5
-						}
+					}
+					// save reference to  details uid to be able restore it from storage
+					if !isPath {
+						blockResult[field.Name+"_details"] = field.Details.PayloadMD5
 					}
 				}
-				if len(blockResult) > 0 && !isPath {
-					task.mx.Lock()
-					if !task.isParsed {
-						task.isParsed = true
-					}
-					task.mx.Unlock()
-					strValue, err := json.Marshal(blockResult)
-					if err != nil {
-						errc <- errs.ParseError{data.url, fmt.Errorf("Fail marshal result: %#v", blockResult)}
+			}
+			if len(blockResult) > 0 && !isPath {
+				task.mx.Lock()
+				if !task.isParsed {
+					task.isParsed = true
+				}
+				task.mx.Unlock()
+				strValue, err := json.Marshal(blockResult)
+				if err != nil {
+					errc <- errs.ParseError{data.url, fmt.Errorf("Fail marshal result: %#v", blockResult)}
+					continue
+				}
+				task.mx.Lock()
+				if zeroPaginator {
+					keyArr := strings.Split(data.key, "-")
+					if len(keyArr) != 2 {
+						errc <- fmt.Errorf("Invalid key: %s", data.key)
 						continue
 					}
-					task.mx.Lock()
-					if zeroPaginator {
-						keyArr := strings.Split(data.key, "-")
-						if len(keyArr) != 2 {
-							errc <- fmt.Errorf("Invalid key: %s", data.key)
-							continue
-						}
-						data.key = fmt.Sprintf("%s-%d", keyArr[0], 0)
-					}
-					recordKey := fmt.Sprintf("%s-%d", data.key, *blockCounter)
-					result <- storage.Record{
-						Key:     recordKey,
-						Type:    storage.INTERMEDIATE,
-						Value:   strValue,
-						ExpTime: 0,
-					}
-					*blockCounter++
-					task.mx.Unlock()
+					data.key = fmt.Sprintf("%s-%d", keyArr[0], 0)
 				}
+				recordKey := fmt.Sprintf("%s-%d", data.key, *blockCounter)
+				select {
+				case <-ctx.Done():
+					return
+				case result <- storage.Record{
+					Key:     recordKey,
+					Type:    storage.INTERMEDIATE,
+					Value:   strValue,
+					ExpTime: 0,
+				}:
+				}
+				*blockCounter++
+				task.mx.Unlock()
 			}
 		}
 	}()
@@ -654,8 +655,8 @@ func (task *Task) saveIntermediate(ctx context.Context, in <-chan storage.Record
 	return errc
 }
 
-func WaitForPipeline(errs ...<-chan error) error {
-	errc := MergeErrors(errs...)
+func waitForPipeline(errs ...<-chan error) error {
+	errc := mergeErrors(errs...)
 	for err := range errc {
 		if err != nil {
 			//return err
@@ -665,7 +666,7 @@ func WaitForPipeline(errs ...<-chan error) error {
 	return nil
 }
 
-func MergeErrors(cs ...<-chan error) <-chan error {
+func mergeErrors(cs ...<-chan error) <-chan error {
 	var wg sync.WaitGroup
 	out := make(chan error, len(cs))
 	output := func(c <-chan error) {
